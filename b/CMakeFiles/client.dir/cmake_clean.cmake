efe7d4d1bb954f894904fec8e9c8ba3dc74e991b
Eliauk-qwe <1552356639@qq.com>
2025-08-01 11:37:50 +0800

    8.1

---
+++ b/CMakeFiles/client.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/client.dir/client/access_group.cpp.o"
+  "CMakeFiles/client.dir/client/access_group.cpp.o.d"
+  "CMakeFiles/client.dir/client/clientmain.cpp.o"
+  "CMakeFiles/client.dir/client/clientmain.cpp.o.d"
+  "CMakeFiles/client.dir/client/file.cpp.o"
+  "CMakeFiles/client.dir/client/file.cpp.o.d"
+  "CMakeFiles/client.dir/client/friend.cpp.o"
+  "CMakeFiles/client.dir/client/friend.cpp.o.d"
+  "CMakeFiles/client.dir/client/friend_quit.cpp.o"
+  "CMakeFiles/client.dir/client/friend_quit.cpp.o.d"
+  "CMakeFiles/client.dir/client/group.cpp.o"
+  "CMakeFiles/client.dir/client/group.cpp.o.d"
+  "CMakeFiles/client.dir/client/group_apply.cpp.o"
+  "CMakeFiles/client.dir/client/group_apply.cpp.o.d"
+  "CMakeFiles/client.dir/client/new_friend.cpp.o"
+  "CMakeFiles/client.dir/client/new_friend.cpp.o.d"
+  "CMakeFiles/client.dir/client/signup.cpp.o"
+  "CMakeFiles/client.dir/client/signup.cpp.o.d"
+  "CMakeFiles/client.dir/client/user.cpp.o"
+  "CMakeFiles/client.dir/client/user.cpp.o.d"
+  "client"
+  "client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
