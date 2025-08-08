b41fb206bc60443e25e3166addc45497175ad909
Eliauk-qwe <1552356639@qq.com>
2025-08-08 17:34:25 +0800

    8.8

---
+++ b/CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server.dir/server/access_group.cpp.o"
+  "CMakeFiles/server.dir/server/access_group.cpp.o.d"
+  "CMakeFiles/server.dir/server/file.cpp.o"
+  "CMakeFiles/server.dir/server/file.cpp.o.d"
+  "CMakeFiles/server.dir/server/friend.cpp.o"
+  "CMakeFiles/server.dir/server/friend.cpp.o.d"
+  "CMakeFiles/server.dir/server/friend_quit.cpp.o"
+  "CMakeFiles/server.dir/server/friend_quit.cpp.o.d"
+  "CMakeFiles/server.dir/server/group.cpp.o"
+  "CMakeFiles/server.dir/server/group.cpp.o.d"
+  "CMakeFiles/server.dir/server/group_apply.cpp.o"
+  "CMakeFiles/server.dir/server/group_apply.cpp.o.d"
+  "CMakeFiles/server.dir/server/new_friend.cpp.o"
+  "CMakeFiles/server.dir/server/new_friend.cpp.o.d"
+  "CMakeFiles/server.dir/server/servermain.cpp.o"
+  "CMakeFiles/server.dir/server/servermain.cpp.o.d"
+  "CMakeFiles/server.dir/server/signup.cpp.o"
+  "CMakeFiles/server.dir/server/signup.cpp.o.d"
+  "CMakeFiles/server.dir/server/user.cpp.o"
+  "CMakeFiles/server.dir/server/user.cpp.o.d"
+  "server"
+  "server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
